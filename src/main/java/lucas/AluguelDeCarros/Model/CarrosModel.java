e4a1d00e7b5cb6ccde5c4dd463d4f8c4da4a2ebf
lucas <lucasaita4000@gmail.com>
2025-07-08 03:27:07 -0300

    feat: Adiciona os models da API

--- src/main/java/lucas/AluguelDeCarros/Model/CarrosModel.java
+++ src/main/java/lucas/AluguelDeCarros/Model/CarrosModel.java
@@ -1,4 +1,53 @@
 package lucas.AluguelDeCarros.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_carros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarrosModel {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "ID")
+private Long id;
+
+@Column(name = "Foto")
+private String imgUrl;
+
+@Column(name = "Modelo")
+@NotBlank(message = "Modelo é Obrigatório" )
+private String modelo;
+
+@Column(name = "Marca")
+@NotBlank(message = "Definir Marca é obrigatório")
+private String marca;
+
+@Column(name = "Cor")
+@NotBlank(message = "Cor é obrigatório")
+private String cor;
+
+@Column(unique = true, name = "Placa")
+@NotBlank(message = "Placa é obrigatório")
+private String placa;
+
+@Column(name = "Ano")
+@NotNull(message = "Ano é obrigatório")
+@Min(value = 2010, message = "Carro muito velho para aluguel")
+private int ano;
+
+@OneToOne
+@JoinColumn(name = "motorista_id", unique = true)
+private MotoristaModel motorista;
+
+
+
 }
--- src/main/java/lucas/AluguelDeCarros/Model/MotoristaModel.java
+++ src/main/java/lucas/AluguelDeCarros/Model/MotoristaModel.java
@@ -1,4 +1,49 @@
 package lucas.AluguelDeCarros.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_motorista")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MotoristaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "Foto")
+    private String imgUrl;
+
+    @Column(name = "Nome")
+    @NotBlank(message = "Obrigatório o nome")
+    private String nomeCompleto;
+
+    @Column(unique = true, name = "Cpf" )
+    @NotBlank(message = "Obrigatório informar o CPF")
+    private String cpf;
+
+    @Column(name = "Data de Nascimento")
+    @NotNull(message = "Obrigatório informar a data de nascimento")
+    private LocalDate dataDeNascimento;
+
+    @Column(unique = true, name = "E-mail")
+    @NotBlank(message = "Obrigatório o email")
+    private String email;
+
+    @OneToOne(mappedBy = "motorista")
+    @JsonIgnore
+    private CarrosModel carro;
+
 }
