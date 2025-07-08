638e3f313f342f99e8d1a99105f5bcc831723969
lucas <lucasaita4000@gmail.com>
2025-07-08 03:43:31 -0300

    style: corrige formatação do código

--- src/main/java/lucas/AluguelDeCarros/Model/MotoristaModel.java
+++ src/main/java/lucas/AluguelDeCarros/Model/MotoristaModel.java
@@ -2,7 +2,6 @@ package lucas.AluguelDeCarros.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
