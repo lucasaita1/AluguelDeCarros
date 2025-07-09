70b7f53c173af5b9507647708cb2307e83b14a06
lucas <lucasaita4000@gmail.com>
2025-07-09 04:31:45 -0300

    fix: corrige as classes controllers com as anotacoes @RestContoller e @RequestMapping.

--- src/main/java/lucas/AluguelDeCarros/Controller/MotoristaController.java
+++ src/main/java/lucas/AluguelDeCarros/Controller/MotoristaController.java
@@ -1,5 +1,11 @@
 package lucas.AluguelDeCarros.Controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/motorista")
 public class MotoristaController {
 
     //CRUD
