488cccc2f0215909bd33385172270d3adb6e1385
lucas <lucasaita4000@gmail.com>
2025-07-14 04:43:54 -0300

    feat: adicionando os controllers funcionas para Carros e Motorista.

--- src/main/java/lucas/AluguelDeCarros/Controller/CarrosController.java
+++ src/main/java/lucas/AluguelDeCarros/Controller/CarrosController.java
@@ -1,20 +1,65 @@
 package lucas.AluguelDeCarros.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lucas.AluguelDeCarros.DTO.CarrosDto;
+import lucas.AluguelDeCarros.Service.CarrosService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/carros")
 public class CarrosController {
 
+    private final CarrosService carrosService;
+
+    public CarrosController(CarrosService carrosService) {
+        this.carrosService = carrosService;
+    }
+
     //CRUD
 
     //#CREAT
+    @PostMapping ("/criar")
+    public ResponseEntity<CarrosDto> adicionarCarro(@RequestBody @Valid CarrosDto carro) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(carrosService.criarCarro(carro));
+    }
 
-    //#READ
+
+    //#READ --> listar todos
+    @GetMapping ("/listar")
+    public ResponseEntity<List<CarrosDto>> listarCarros() {
+        return ResponseEntity.ok(carrosService.listarCarros());
+    }
+
+    //#READ --> Listar por ID
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<CarrosDto> buscarPorId(@PathVariable Long id) {
+        Optional<CarrosDto> carroId = carrosService.listarCarrosId(id);
+
+        return carroId
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
     //#UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<CarrosDto> carroAtualizado(@PathVariable Long id, @RequestBody @Valid CarrosDto dto) {
+        return ResponseEntity.ok(carrosService.editarCarro(id, dto));
+    }
 
     //#DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCarro (@PathVariable Long id) {
+        carrosService.deletarCarro(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/lucas/AluguelDeCarros/Controller/MotoristaController.java
+++ src/main/java/lucas/AluguelDeCarros/Controller/MotoristaController.java
@@ -1,20 +1,62 @@
 package lucas.AluguelDeCarros.Controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lucas.AluguelDeCarros.DTO.MotoristaDto;
+import lucas.AluguelDeCarros.Service.MotoristaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/motorista")
 public class MotoristaController {
 
+    private MotoristaService motoristaService;
+
+    public MotoristaController(MotoristaService motoristaService) {
+        this.motoristaService = motoristaService;
+    }
+
     //CRUD
 
+
     //#CREAT
+    @PostMapping("/criar")
+    public ResponseEntity<MotoristaDto> adicionarMotorista(@RequestBody @Valid MotoristaDto motoristaDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(motoristaService.criarMotorista(motoristaDto));
+    }
 
     //#READ
+    @GetMapping("/listar")
+    public ResponseEntity<List<MotoristaDto>> listarMotoristas() {
+        return ResponseEntity.ok(motoristaService.listarMotoristas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MotoristaDto> buscarPorId(@PathVariable Long id) {
+        Optional<MotoristaDto> carroId = motoristaService.listarMotoristaPorID(id);
+
+        return carroId
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     //#UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<MotoristaDto> atualizar(@PathVariable Long id, @RequestBody @Valid MotoristaDto motoristaDto) {
+        return ResponseEntity.ok(motoristaService.editarMotorista(id, motoristaDto));
+    }
 
     //#DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarMotorista(@PathVariable Long id) {
+        motoristaService.deletarMotorista(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
